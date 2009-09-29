34bf18109f2a7b20c5ec3dc71df7e7eff6e48619
ciprian.caloianu@gmail.com <ciprian.caloianu@gmail.com@177cb622-9be5-11de-bf47-b33d4a03a5a8>
2009-09-29 12:46:58 +0000

    Ported sqlite to android

--- trunk/framework/src/org/pannotas/Main.java
+++ trunk/framework/src/org/pannotas/Main.java
@@ -5,14 +5,21 @@ package org.pannotas;
  *
  */
 public class Main {
-
-
+	RepositorySqlite rep = new RepositorySqlite();
+	
+	
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		Main m = new Main();
+		m.run();
+	}
+	
+	public void run() throws Exception {
 		System.out.println("Hello PanNotas!");
-		
+		rep.open("test.db");
+		rep.close();
 	}
 
 }
